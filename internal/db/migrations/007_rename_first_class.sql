db107cd8917edfa34c3c87efd2e81bb09c6bddf0
seastco <seastcott@outlook.com>
2026-03-24 23:20:44 -0400

    fix migration

--- internal/db/migrations/007_rename_first_class.sql
+++ internal/db/migrations/007_rename_first_class.sql
@@ -1,5 +1,5 @@
 -- Rename shipping tier "first_class" to "standard"
-UPDATE messages SET shipping_tier = 'standard' WHERE shipping_tier = 'first_class';
 ALTER TABLE messages DROP CONSTRAINT messages_shipping_tier_check;
+UPDATE messages SET shipping_tier = 'standard' WHERE shipping_tier = 'first_class';
 ALTER TABLE messages ADD CONSTRAINT messages_shipping_tier_check
     CHECK (shipping_tier IN ('standard', 'priority', 'express'));
